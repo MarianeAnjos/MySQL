62e92ab6ded39d8855b1306364886b644fd1af98
Mariane Anjos <100323487+MarianeAnjos@users.noreply.github.com>
2023-04-10 11:31:26 -0300

    Exercicios de modelagem de dados

---
+++ MySQL Exercicio Quitanda.sql
@@ -0,0 +1,94 @@
+CREATE DATABASE db_sacolao;
+
+USE db_sacolao;
+
+CREATE TABLE tb_categorias(
+id bigint AUTO_INCREMENT,
+descricao VARCHAR(255) NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE tb_produtos(
+id bigint AUTO_INCREMENT,
+
+nome VARCHAR(255) NOT NULL,
+quantidade INT,
+dtvalidade DATE NULL,
+preco DECIMAL(6,2),
+categorias_id BIGINT,
+PRIMARY KEY (id),
+FOREIGN KEY (categorias_id) REFERENCES tb_categorias(id)
+); 
+
+INSERT INTO tb_categorias (descricao)
+VALUES ("Frutas");
+
+INSERT INTO tb_categorias (descricao)
+VALUES ("Verduras");
+
+INSERT INTO tb_categorias (descricao)
+VALUES ("Legumes");
+
+INSERT INTO tb_categorias (descricao)
+VALUES ("Temperos");
+
+INSERT INTO tb_categorias (descricao)
+VALUES ("Ovos");
+
+INSERT INTO tb_categorias (descricao)
+VALUES ("Outros");
+
+SELECT*FROM tb_categorias;
+
+INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categorias_id)
+VALUES ("Maçã", 1000, "2022-03-07", 1.99, 1);
+
+INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categorias_id)
+VALUES ("Banana", 1300, "2022-03-08", 5.00, 1);
+
+INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categorias_id)
+VALUES ("Batata doce", 2000, "2022-03-09", 10.00, 3);
+
+INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categorias_id)
+VALUES ("Alface", 300, "2022-03-10", 7.00, 2);
+
+INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categorias_id)
+VALUES ("Cebola", 1020, "2022-03-08", 5.00, 3);
+
+INSERT INTO tb_produtos(nome, quantidade, dtvalidade, preco, categorias_id)
+VALUES("Ovo Branco", 1000, "2022-03-07", 15.00, 5);
+
+INSERT INTO tb_produtos(nome, quantidade, dtvalidade, preco, categorias_id)
+VALUES("Agrião", 1500, "2022-03-06", 3.00, 2);
+
+INSERT INTO tb_produtos(nome, quantidade, dtvalidade, preco, categorias_id)
+VALUES("Cenoura", 1800, "2022-03-09", 3.50, 3);
+
+INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categorias_id)
+VALUES ("Pimenta", 1100, "2022-03-15", 10.00, 4);
+
+INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categorias_id)
+VALUES ("Alecrim", 130, "2022-03-10", 5.00, 4);
+
+INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categorias_id)
+VALUES ("Manga", 200, "2022-03-07", 5.49, 1);
+
+INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categorias_id)
+VALUES ("Laranja", 3000, "2022-03-13", 10.00, 1);
+
+INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categorias_id)
+VALUES ("Couve", 100, "2022-03-12", 1.50, 2);
+
+INSERT INTO tb_produtos(nome, quantidade, dtvalidade, preco, categorias_id)
+VALUES("Tomate", 1105, "2022-03-15", 3.00, 3);
+
+INSERT INTO tb_produtos(nome, quantidade, dtvalidade, preco, categorias_id)
+VALUES("Rabanete", 1200, "2022-03-15", 13.00, 3);
+
+INSERT INTO tb_produtos(nome, quantidade, preco)
+VALUES("Sacola Cinza", 1118, 0.50);
+
+INSERT INTO tb_produtos(nome, quantidade, preco)
+VALUES("Sacola Verde", 1118, 0.50);
+
+SELECT * FROM tb_produtos ORDER BY quantidade;
